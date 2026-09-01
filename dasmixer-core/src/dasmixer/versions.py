718ae1a38192a5529b02918cc8a593fdf378d6e7
gluck <glucksistemi@gmail.com>
2026-09-02 00:48:51 +0300

    0.7.2a2 version

--- dasmixer-core/src/dasmixer/versions.py
+++ dasmixer-core/src/dasmixer/versions.py
@@ -1,7 +1,7 @@
 """Version constants for DASMixer project."""
 
 # Версия приложения DASMixer
-APP_VERSION = "0.7.2a1"
+APP_VERSION = "0.7.2a2"
 
 # Версия схемы файла проекта (.dasmix).
 # Поднимается только тогда, когда меняется схема БД.
