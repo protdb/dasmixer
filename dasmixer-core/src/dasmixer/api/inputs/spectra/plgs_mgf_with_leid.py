ab0f3660f116cb1bb366fa2f72aad0a30373f673
Kirill <dev@psskb.org>
2026-09-22 10:07:33 +0300

    требования 0.8.0 апдейт; fix plgs_mgf_with_leid.py parser regexp

--- dasmixer-core/src/dasmixer/api/inputs/spectra/plgs_mgf_with_leid.py
+++ dasmixer-core/src/dasmixer/api/inputs/spectra/plgs_mgf_with_leid.py
@@ -4,4 +4,4 @@ from .mgf import MGFParser
 
 
 class MGFParserPLGS(MGFParser):
-    scan_regexp = re.compile(r'lepeakid:(\d+):', re.IGNORECASE)
+    scan_regexp = re.compile(r'lepeakid:(\d+)', re.IGNORECASE)
